05a2a200a4b0ab1127b9df838571a8fa507e24ca
ayushmp10 <ayushparambath@gmail.com>
2025-07-02 13:36:27 -0400

    built extract amount value function, edited investment to provide multiple formatted suggestions

--- investment.py
+++ investment.py
@@ -12,27 +12,44 @@ class Investment:
             print(f"Error fetching data: {e}")
             return None
 
-    def get_biggest_gainer(self, data):
+    def get_biggest_gainers(self, data, amount):
         if not data:
             return None
-        return data[0] # return the first stock in the list which is the biggest gainer in this api structure
+        stocks = []
+        for stock in data:
+            if stock['price'] <= amount:
+                stocks.append(stock)
+                if len(stocks) == 5:
+                    break
+        return stocks
 
     def format_stock_info(stock_info):
         if not stock_info:
             return "No stock data available."
         return (
-            f"This is the biggest stock gainer: {stock_info['name']} Ticker: {stock_info['symbol']}\n"
-            f"This is the price of the stock: {stock_info['price']}\n"
-            f"This is the change percent of the stock: {stock_info['changesPercentage']}\n"
+            f"{stock_info['name']:<35} "
+            f"{stock_info['symbol']:<10} "
+            f"{stock_info['price']:<10} "
+            f"{stock_info['changesPercentage']:<15}"
         )
-
+    
     def print_stock_info(self, stock):
         print(Investment.format_stock_info(stock))
+
+import re
+
+def extract_amount(input_string):
+    match = re.search(r'amount:\s*([0-9]+(?:\.[0-9]+)?)', input_string)
+    if match:
+        return float(match.group(1))
+    return None
 
 if __name__ == "__main__":
     api_key = "5H9LohX4eRJoAHXeYftqgRrd2UrWkRdW"
     url = f"https://financialmodelingprep.com/stable/biggest-gainers?apikey=5H9LohX4eRJoAHXeYftqgRrd2UrWkRdW"
     investment = Investment(api_key)
     data = investment.make_request(url)
-    stock = investment.get_biggest_gainer(data)
-    investment.print_stock_info(stock)
+    stocks = investment.get_biggest_gainers(data, 50)
+    print(f"{'Company':<35} {'Ticker':<10} {'Price':<10} {'Change Percent':<15}")
+    for stock in stocks:
+        investment.print_stock_info(stock)
