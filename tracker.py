042ca1a0140fa6b6080cb95b234a6958898eb80b
Mohammed Bari <mohammedbari@mac.mynetworksettings.com>
2025-07-01 22:01:20 -0400

    completed gemini model

--- tracker.py
+++ tracker.py
@@ -1,2 +1,20 @@
 import os
+from google import genai
+from google.genai import types
 
+my_api_key = os.getenv('GENAI_KEY')
+genai.api_key = my_api_key
+
+client = genai.Client(
+    api_key=my_api_key,
+)
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    config=types.GenerateContentConfig(
+      system_instruction="You are acting as a financial advisor, give good financial decisions and reccomendations for a person"
+    ),
+    contents="Based off the following datatable of expenses, provide reccomendations to this person for budgeting and their expenses",
+)
+
+print(response.text)
