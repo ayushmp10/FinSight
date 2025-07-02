3bad8124a587b6e130b1412895515076792a4ade
Mohammed Bari <mohammedbari@mac.mynetworksettings.com>
2025-07-02 00:02:14 -0400

    worked more on SQL

--- tracker.py
+++ tracker.py
@@ -1,6 +1,7 @@
 import os
 from google import genai
 from google.genai import types
+import sqlite3
 
 my_api_key = os.getenv('GENAI_KEY')
 genai.api_key = my_api_key
@@ -9,12 +10,24 @@ client = genai.Client(
     api_key=my_api_key,
 )
 
-response = client.models.generate_content(
+def ask_gemini(prompt):
+    response = client.models.generate_content(
     model="gemini-2.5-flash",
     config=types.GenerateContentConfig(
       system_instruction="You are acting as a financial advisor, give good financial decisions and reccomendations for a person"
     ),
-    contents="Based off the following datatable of expenses, provide reccomendations to this person for budgeting and their expenses",
-)
+    contents=prompt
+    )
+    return response.text
 
-print(response.text)
+
+conn = sqlite3.connect('expenses.db')
+c = conn.cursor()
+c.execute(
+    "CREATE TABLE IF NOT EXISTS expenses (category TEXT, amount REAL, date TEXT)"
+
+)
+conn.commit()
+conn.close()
+
+def insert_into_table(category, amount, date):
